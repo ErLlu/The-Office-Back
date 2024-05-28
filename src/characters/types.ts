7802833205f0deead41d85deb24d7ce29ea0122d
Eric Lluis <ericlluis84@gmail.com>
2024-05-28 18:36:57 +0200

    Create type CharacterStructure

---
+++ src/characters/types.ts
@@ -0,0 +1,10 @@
+type CharacterStructure = {
+  name: string;
+  age: number;
+  position: string;
+  state: boolean;
+  urlImage: string;
+  alternativeText: string;
+  description: string;
+};
+export default CharacterStructure;
--- src/server/app.ts
+++ src/server/app.ts
@@ -1,12 +1,3 @@
-import chalk from "chalk";
 import express from "express";
 
 export const app = express();
-
-export const servServer = (port: number) => {
-  app.listen(port, () => {
-    console.log(`Listening on ${chalk.green(`http://localhost:${port}`)}`);
-  });
-};
-
-export default servServer;
