7f8a58c1ede748e1444b6b795e9b11e92be3cfe8
Eric Lluis <ericlluis84@gmail.com>
2024-05-29 00:07:42 +0200

    Connect to The Office data base

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import "dotenv/config";
 import startServer from "./server/index.js";
+import connectToDataBase from "./database/index.js";
 
 const port = process.env.PORT ?? 8001;
+
+const dataBaseUri = process.env.MONGODB_URI!;
+await connectToDataBase(dataBaseUri);
 
 startServer(port as number);
