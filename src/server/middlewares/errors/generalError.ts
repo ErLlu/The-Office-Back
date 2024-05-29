508980ccbd0e63dd82a1839e8b8e984ddb0675d4
Eric Lluis <ericlluis84@gmail.com>
2024-05-29 13:06:47 +0200

    Add generalError middleware

---
+++ src/server/middlewares/errors/generalError.ts
@@ -0,0 +1,19 @@
+import { type NextFunction, type Request, type Response } from "express";
+import type ServerError from "../ServerError/ServerError";
+import chalk from "chalk";
+
+const generalError = (
+  error: ServerError,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+): void => {
+  const statusCode = error.statusCode ?? 500;
+  const errorMessage = error.message || "Server failed: Unknown error";
+
+  console.log(chalk.red(`${error.message}`));
+
+  res.status(statusCode).json({ error: `${errorMessage}` });
+};
+
+export default generalError;
